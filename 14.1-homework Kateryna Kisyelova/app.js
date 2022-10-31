26b77ea669949cc9d1e37a8933ccb46f47a7497b
Kate <katekiseleva88@gmail.com>
2022-11-01 01:56:31 +0200

    add hw-14

---
+++ 14.1-homework Kateryna Kisyelova/app.js
@@ -0,0 +1,46 @@
+class Student {
+  constructor(name, marks) {
+    this.name = name;
+    this.marks = marks;
+  }
+
+  getAverageMark() {
+    return this.getMarksSum() / this.marks.length;
+  }
+
+  getMarksSum() {
+    return this.marks.reduce((sum, mark) => (sum += mark));
+  }
+}
+
+class Group {
+  students = [];
+
+  addStudent(student) {
+    if (this.isStudent(student)) {
+      const averageMark = student.getAverageMark();
+      this.students.push(averageMark);
+      return averageMark;
+    }
+  }
+
+  isStudent(student) {
+    return student instanceof Student;
+  }
+
+  getAverageMark() {
+    return this.getAverageMarksSum() / this.students.length;
+  }
+
+  getAverageMarksSum() {
+    return this.students.reduce((sum, mark) => (sum += mark));
+  }
+}
+const group = new Group();
+
+group.addStudent(new Student("John", [10, 8]));
+group.addStudent(new Student("Alex", [10, 9]));
+group.addStudent(new Student("Bob", [6, 10]));
+group.addStudent({});
+
+console.log(group.getAverageMark() === (9 + 9.5 + 8) / 3);
