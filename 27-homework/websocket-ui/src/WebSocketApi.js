8771ae676732d1aa64f3f68febba54dd0dde1dbb
Kate <katekiseleva88@gmail.com>
2022-12-16 23:56:09 +0200

    adding hw-27

---
+++ 27-homework/websocket-ui/src/WebSocketApi.js
@@ -0,0 +1,29 @@
+export default class WebSocketApi {
+  constructor(url, options) {
+    this.options = options;
+    this.ws = new WebSocket(url);
+    this.ws.onopen = this.onOpen.bind(this);
+    this.ws.onmessage = this.onMessage.bind(this);
+    this.ws.onerror = this.onError.bind(this);
+  }
+
+  send(message) {
+    this.ws.send(JSON.stringify(message));
+  }
+
+  onOpen() {
+    console.log("Connection with server was established");
+
+    this.ws.send(
+      JSON.stringify({ name: "server", message: "New client connected" })
+    );
+  }
+
+  onMessage(e) {
+    this.options.onMessage(JSON.parse(e.data));
+  }
+
+  onError() {
+    
+  }
+}
